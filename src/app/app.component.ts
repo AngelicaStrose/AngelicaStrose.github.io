71f988621f622c34f2c1b470d348dc4aa7346b4e
arose42 <arose42@gatech.edu>
2018-09-28 17:01:31 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import {HeaderComponent } from './header/app.headerComponent';
+
+import { NavComponent } from './nav/app.navComponent';
+
+import { ContentAreaComponent} from './contentArea/app.contentAreaComponent';
+
+import { FooterComponent} from './footer/footerComponent';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent {
+
+
+}
---
+++ src/app/contentArea/app.contentAreaComponent.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './contentArea.html',
+})
+
+export class ContentAreaComponent {
+
+
+}
